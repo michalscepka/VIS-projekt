cfa3c8ebca0fa12fce8f8cca0b6e9f9e43a9e9b4
michalscepka <scepkamichal1@gmail.com>
2020-11-30 23:27:33 +0100

    CMD test created

---
+++ CMDTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using BusinessLayer.BO;
+
+namespace CMDTest
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Zakaznik uzivatel = new Zakaznik
+			{
+				Jmeno = "Petr"
+			};
+			Console.WriteLine(uzivatel.Jmeno);
+		}
+	}
+}
